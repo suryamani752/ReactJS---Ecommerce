9a9eb228bf8e3020ae01e7737413e1f2d6fff6b0
Suryamani kumar <124435021+suryamani752@users.noreply.github.com>
2025-09-17 19:36:48 +0530

    modified Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
+            {/* brand name */}
             <Link to="/" className="text-2xl font-bold text-gray-900">
               CodeYodha-shop
             </Link>
